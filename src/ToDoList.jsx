336935830956adffdbdbb81cf5cdd9094d6632a4
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-10 19:46:32 +0600

    handle input

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -5,6 +5,39 @@ function ToDoList(){
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState([]);
 
-  return(<></>)
+  function handleInputChange(event){
+    setNewTask(event.target.value);
+  }
+
+  function addTask(){
+
+  }
+
+  function deleteTask(index){
+
+  }
+
+  function moveTaskUp(index){
+
+  }
+
+  function moveTaskDown(index){
+
+  }
+
+  return( 
+  <div className='to-do-list'>
+
+    <h1>To-Do-List</h1>
+    <div>
+      <input 
+          type="text" 
+          placeholder='Enter a task ...'
+          value={newTask}
+          onChange={handleInputChange} />
+
+    </div>
+
+  </div>)
 }
 export default ToDoList
