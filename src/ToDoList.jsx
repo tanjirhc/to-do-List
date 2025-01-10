c91e721cc386f1137d4e12e2abf3bff34ce8f160
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-10 19:38:35 +0600

    ToDO List state variable

---
+++ src/ToDoList.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+function ToDoList(){
+
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState([]);
+
+  return(<></>)
+}
+export default ToDoList
